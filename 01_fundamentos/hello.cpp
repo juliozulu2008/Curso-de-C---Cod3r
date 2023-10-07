02ea016d7209a6325a0bd23c31ffaf00cd75d6b0
Julio Cesar Dias Dos Santos <juliozulu2008@gmail.com>
2023-10-07 14:19:45 -0300

    Curso de C++

---
+++ 01_fundamentos/hello.cpp
@@ -0,0 +1,8 @@
+// a funçao main é a porta de entrada de um programa em C++
+#include <iostream>
+
+int main()
+{ // os os retornam numero para o compilador por isso a funçao é declarada com um inteiro
+    std::cout << "Hello world!!";
+    return 0;
+}
